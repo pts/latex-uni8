d2666a9866ee38dc8a91a23f725dc883a8b05811
Peter Szabo <pts@fazekas.hu>
2019-01-14 16:06:30 +0100

    fixed test.sh for recently added example_*.tex files

--- test.sh
+++ test.sh
@@ -3,12 +3,18 @@
 set -ex
 
 for F in example*.tex; do
-  rm -f "${F%.*}.aux"
-  if test "$F" = example_pm.tex; then
+  rm -f "${F%.*}.aux" "${F%.*}.log" "${F%.*}.pdf"
+  DO_LUALATEX=1
+  case "$F" in example_p*r.tex) DO_LUALATEX= ; esac
+  WORKS_IN_PDFLATEX=1
+  case "$F" in example_f*.tex) WORKS_IN_PDFLATEX= ; esac
+  DO_MULTIRUN=
+  case "$F" in example_*m*.tex) DO_MULTIRUN=1 ; esac
+  if test "$DO_MULTIRUN"; then
     # Generate .aux file with labels.
     pdflatex --interaction=nonstopmode "$F" ||:
   fi
-  if test "${F#example_f}" = "$F"; then  # Works in pdflatex.
+  if test "$WORKS_IN_PDFLATEX"; then  # Works in pdflatex.
     pdflatex --interaction=nonstopmode "$F"
     grep 'Warning: ' "${F%.*}".log && exit 1
   else
@@ -19,13 +25,15 @@ for F in example*.tex; do
     fi
   fi
 
-  rm -f "${F%.*}.aux"
-  if test "$F" = example_pm.tex; then
-    # Generate .aux file with labels.
-    lualatex --interaction=nonstopmode "$F" ||:
+  if test "$DO_LUALATEX"; then
+    rm -f "${F%.*}.aux" "${F%.*}.log" "${F%.*}.pdf"
+    if test "$DO_MULTIRUN"; then
+      # Generate .aux file with labels.
+      lualatex --interaction=nonstopmode "$F" ||:
+    fi
+    lualatex --interaction=nonstopmode "$F"
+    grep 'Warning: ' "${F%.*}".log && exit 1
   fi
-  lualatex --interaction=nonstopmode "$F"
-  grep 'Warning: ' "${F%.*}".log && exit 1
 done
 
 : test.sh OK.
